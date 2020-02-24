473ee9607751b3a75e3c0a7b38095b445e8baac9
Harsh Patel <harshbpatel97@gmail.com>
2020-02-23 20:56:50 -0500

    Added MySql client connections

--- HangryBirds/settings.py
+++ HangryBirds/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'HangryBirds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Restaurants',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '34.73.45.104',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
